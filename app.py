4d22d36af04b14927462e53feec3faa323e7ff67
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-24 05:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
-from diffusers import DiffusionPipeline
 import torch
+from diffusers import (
+    StableDiffusionXLPipeline, 
+    EulerAncestralDiscreteScheduler,
+    AutoencoderKL
+)
 from io import BytesIO
 import base64
-from huggingface_hub import snapshot_download
 import os
+
 
 class InferlessPythonModel:
     def initialize(self):
-        self.pipe = DiffusionPipeline.from_pretrained(
-            "playgroundai/playground-v2.5-1024px-aesthetic",
-            use_safetensors=True,
-            torch_dtype=torch.float16,
-            variant="fp16"
-        ).to("cuda")
+        # Load VAE component
+        self.vae = AutoencoderKL.from_pretrained(
+            "madebyollin/sdxl-vae-fp16-fix", 
+            torch_dtype=torch.float16
+        )
+        
+        # Configure the pipeline
+        self.pipe = StableDiffusionXLPipeline.from_pretrained(
+            "cagliostrolab/animagine-xl-3.0", 
+            vae=self.vae,
+            torch_dtype=torch.float16, 
+            use_safetensors=True, 
+        )
+        self.pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(self.pipe.scheduler.config)
+        self.pipe.to('cuda')
+
 
     def infer(self, inputs):
         prompt = inputs["prompt"]
-        image = self.pipe(prompt, num_inference_steps=50, guidance_scale=3).images[0]
+        negative_prompt = inputs["negative_prompt"]
+        
+        image = pipe(
+            prompt, 
+            negative_prompt=negative_prompt, 
+            width=832,
+            height=1216,
+            guidance_scale=7,
+            num_inference_steps=28
+        ).images[0]
+
         buff = BytesIO()
         image.save(buff, format="JPEG")
         img_str = base64.b64encode(buff.getvalue()).decode()
