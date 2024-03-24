b81f1a9c1964858105b758968ff5d4b88ecc0b38
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-24 05:53:18 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,12 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["Penguins having dinner"]
+        'example': ["1girl, arima kana, oshi no ko, solo, upper body, v, smile, looking at viewer, outdoors, night"]
+    },
+    "negative_prompt": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name"]
     }
 }
